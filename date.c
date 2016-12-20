24c11a95c5bd29413b792f998fe020ab1426ca39
Vic James V. Mesina <mesina_vicjames03@yahoo.com>
2016-12-20 09:13:08 +0800

    Create date.c

---
+++ date.c
@@ -0,0 +1,68 @@
+int main(){
+int month, day;
+    
+    printf("Enter Month:");
+    scanf("%d", &month); 
+    
+    printf("Enter day:");
+    scanf("%d", &day);
+    if(month == 1){
+    printf("January");
+}
+    if(month == 2){
+    printf("Febuary");
+}
+    if(month == 3){
+    printf("March");
+}
+    if(month == 4){
+    printf("April");
+}
+    if(month == 5){
+    printf("May");
+}
+    if(month == 6){
+    printf("June");
+}
+    if(month == 7){
+    printf("July");
+}
+    if(month == 8){
+    printf("August");
+}
+    if(month == 9){
+    printf("September");
+}
+    if(month == 10){
+    printf("October");
+}
+    if(month == 11){
+    printf("Novemebr");
+}
+    if(month == 12){
+    printf("December");
+}
+    if(day == 1){
+    printf("Monday");
+}
+    if(day == 2){
+    printf("Tuesday");
+}
+    if(day == 3){
+    printf("Wendsday");
+}
+    if(day == 4){
+    printf("Thursday");
+}
+    if(day == 5){
+    printf("Friday");
+}
+    if(day == 6){
+    printf("Saturday");
+}
+    if(day == 7){
+    printf("Sunday");
+}
+}
+
+ 
