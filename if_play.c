a4764d7c608e7a25180d881c0d9f6268b936f87e
Vic James V. Mesina <mesina_vicjames03@yahoo.com>
2016-12-20 08:32:38 +0800

    Create if_play.c

---
+++ if_play.c
@@ -0,0 +1,16 @@
+#include <iostream>
+int main(){
+	int num;
+	
+	printf("Enter num: ");
+	scanf("%d", &num);
+	if(num%3 == 0){
+		printf("Pogi");
+	}else{
+	if(num%5 == 0){
+		printf("Ed");
+	}else{
+	if(num%3,5 == 0);
+	}printf("Ed Pogi");
+	}
+}
