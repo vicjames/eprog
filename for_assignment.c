dd080f6cda39261c96829daa05c353f6d3238ba4
Vic James V. Mesina <mesina_vicjames03@yahoo.com>
2017-01-03 06:12:49 +0800

    Create for_assignment.c

---
+++ for_assignment.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  
+  system("PAUSE");	
+  return 0;
+  int i = 20;
+ 
+while (i %  2 != 0 || i %  3 != 0 || i %  4 != 0 || i %  5 != 0 ||
+         i %  6 != 0 || i %  7 != 0 || i %  8 != 0 || i %  9 != 0 ||
+         i % 10 != 0 || i % 11 != 0 || i % 12 != 0 || i % 13 != 0 ||
+         i % 14 != 0 || i % 15 != 0 || i % 16 != 0 || i % 17 != 0 ||
+         i % 18 != 0 || i % 19 != 0 || i % 20 != 0) {
+    i += 20;
+}
